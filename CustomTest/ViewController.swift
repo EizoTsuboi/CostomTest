1bcc27f0e718fd4f90c8cd9382e6c69d4a49390c
EizoTsuboi <eizo.wakuwaku@gmail.com>
2019-08-17 20:13:28 +0900

    fix xib

--- CustomTest/ViewController.swift
+++ CustomTest/ViewController.swift
@@ -13,8 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      
     }
-
+    @IBAction func viewCostom(_ sender: Any) {
+        let costomView: CostomView = CostomView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        self.view.addSubview(costomView)
+        costomView.translatesAutoresizingMaskIntoConstraints = false
+        costomView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300.0).isActive = true
+        costomView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        print(costomView.frame.size.height)
+        print(costomView.frame.size.width)
+      
+    }
+    
 
 }
 
